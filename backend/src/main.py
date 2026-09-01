9f800a5d40037d0c4b06f8f0f7fe0b0e7962eb89
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-09-01 16:22:35 +0000

    Move main.py into src directory

---
+++ backend/src/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def home():
+    return {"message": "Welcome to my blog"}
+
+
+@app.get("/health")
+async def check_health():
+    return {"status": "ok"}
